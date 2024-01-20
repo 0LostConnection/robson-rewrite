aade39d1069314ebb17957dfab2c935873b9da6a
Geovane <geovane.saraiva.silva75@gmail.com>
2024-01-20 20:11:48 -0300

    Now, commands work!

---
+++ src/events/interactionCreate.js
@@ -0,0 +1,22 @@
+import EventStructure from "../components/structures/EventStructure.js"
+
+export default class extends EventStructure {
+    constructor(client) {
+        super(client, {
+            name: 'interactionCreate'
+        })
+    }
+
+    run = (interaction) => {
+        if (!interaction.isChatInputCommand()) return
+
+        const command = this.client.commandsList.get(interaction.commandName)
+        if (!command) return
+
+        try {
+            command.run(interaction)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+}
