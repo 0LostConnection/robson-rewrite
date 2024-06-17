9da3c2931edab585c2aacd4032b3f69bda7c24f1
Geovane Saraiva da Silva <70657835+0LostConnection@users.noreply.github.com>
2024-06-16 22:49:14 -0300

    Clean: cleaning some unnecessary files

--- removeCommand.js
+++
@@ -1,4 +0,0 @@
-import 'dotenv/config'
-import RemoveCommands from "./src/core/infra/RemoveCommand";
-
-new RemoveCommands(process.env.BOT_TOKEN, process.env.CLIENT_ID).removeGuildCommand('1014555852469964920', '1200187909102387260')
